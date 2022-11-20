c530f3b6cdbf46f976e35f6a4ea942d110210c36
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:11:00 +0300

    Add files via upload

---
+++ 6.103/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _6._103
+{
+    class Program
+    {
+        static double Metod(string Name, double Min = double.MinValue, double Max = double.MaxValue, double E2 = double.MinValue)
+        {
+            double Num;
+            Console.WriteLine($"Введите {Name}:");
+            while (!(double.TryParse(Console.ReadLine(), out Num) && Num > Min && Num < Max && Num != E2)) Console.WriteLine($"Неверно {Name}");
+            return Num;
+        }
+        static void Main(string[] args)
+        {
+            int a = (int)Metod("Числитель");
+            int b = (int)Metod("Знаменатель");
+            int a1 = 0;
+            int b2 = 0;
+            if (a > b) { a1 = a; b2 = b; }
+            else { a1 = b; b2 = a; }
+            while (a1 != 0)
+            {
+                if (a1 < b2) { b2 -= a1; continue; }
+                a1 -= b2;
+            }
+            int z = a / b2;
+            int x = b / b2;
+            Console.WriteLine(b2 == 1 ? "Дроби не сокращаются" : $"{a}/{b} = {z}/{x}");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
